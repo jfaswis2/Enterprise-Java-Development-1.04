e6ef3cb8ac050220bda198edf0f2119d5cfdefed
Jhon <jfaswis2@gmail.com>
2022-08-02 14:42:44 +0200

    Primer ejercicio OK.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] num = {5,9,3,4,15,2,2,5,1};
+        mayorMenor(num);
     }
+
+    public static void mayorMenor(int[] num){
+
+        if(num.length>= 1) {
+
+            int numMayor = 0;
+            int numMenor = num[0];
+
+            for (int i = 0; i < num.length; i++) {
+                if (num[i] >= numMayor) {
+                    numMayor = num[i];
+                }
+
+                if (num[i] <= numMenor) {
+                    numMenor = num[i];
+                }
+            }
+            System.out.println("Numero mayor: " + numMayor + "\n" + "Numero menor: "
+                    + numMenor);
+            System.out.println("La diferencia es de: " + (numMayor - numMenor));
+        } else{
+            System.out.println("La longitud del array debe ser igual o mayor a 1");
+        }
+    }
+
+    public static void primeroSegundo(){
+
+    }
+
 }
